31032a715a15637c6fef066bcaad21f419e7cdd3
Jan Kolařík <kolarikjan02@gmail.com>
2023-04-04 01:04:03 +0200

    components lecture

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
 
@@ -32,10 +32,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} price={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} price={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} price={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} price={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      <Expenses data={expenses}/>
     </div>
   );
   
--- first-app/src/components/ExpenseItem.js
+++ first-app/src/components/ExpenseItem.js
@@ -1,17 +1,18 @@
 import './ExpenseItem.css';
 
 import ExpanseDate from './ExpenseDate';
+import Card from './Card';
 
 function ExpenseItem(props) {
 
     return (
-        <div className="expense-item">
+        <Card className="expense-item">
             <ExpanseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
                 <span className='expense-item__price'>{props.price} $</span>
             </div>
-        </div>
+        </Card>
     );
     
 }
